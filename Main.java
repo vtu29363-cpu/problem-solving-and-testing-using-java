832d785a3130cbbc1905ee178d7b328ebea2a87f
Bolli Shashank <shashankbolli06@gmail.com.com>
2026-08-13 09:59:22 +0530

    Add new Java problem

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+
+            currentSum += num;
+            maxSum = Math.max(maxSum, currentSum);
+
+            if (currentSum < 0)
+                currentSum = 0;
+        }
+
+        System.out.println(maxSum);
+    }
+}
