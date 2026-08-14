e7081549a553c52c7a799c09dd9dc70e175770f1
Bolli Shashank <shashankbolli06@gmail.com.com>
2026-08-14 11:47:47 +0530

    CalculatorUnitTesting.java

---
+++ CalculatorUnitTesting.java
@@ -0,0 +1,28 @@
+public class CalculatorUnitTesting {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        // Addition
+        int addition = a + b;
+
+        if (addition == 30) {
+            System.out.println("Addition Test Passed.");
+        } else {
+            System.out.println("Addition Test Failed.");
+        }
+
+        // Division
+        int x = 10;
+        int y = 2;
+        int division = x / y;
+
+        if (division == 5) {
+            System.out.println("Division Test Passed.");
+        } else {
+            System.out.println("Division Test Failed.");
+        }
+    }
+}
